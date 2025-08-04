fc0257610e7d3fa193f6a5105a64fc56dd27266d
alllexey <makarovalesha0212@gmail.com>
2025-08-04 14:10:55 +0800

    Initial commit

---
+++ src/main/java/me/alllexey123/openjfs/OpenJFSApplication.java
@@ -0,0 +1,13 @@
+package me.alllexey123.openjfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenJFSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenJFSApplication.class, args);
+	}
+
+}
